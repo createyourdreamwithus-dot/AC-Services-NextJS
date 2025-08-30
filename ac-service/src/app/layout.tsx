6ce38d739332eb425b984f6581f14afb6167ca3a
Vigneshwar01 <vigneshwar@i2global.io>
2025-08-30 21:19:59 +0530

    project setup

---
+++ ac-service/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Josefin_Sans } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/Components/Navbar/Navbar";
+import Footer from "@/Components/Footer/Footer";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+
+export const metadata: Metadata = {
+  title: "Ac Service",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={josefin.className}>
+    <body className="bg-gray-50 text-gray-900 flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+
+    </body>
+  </html>
+  );
+}
