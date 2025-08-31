21dd3ea746f58a2d1b457375ccb9557faf5e6066
vincent-18-dev <154895414+vincent-18-dev@users.noreply.github.com>
2025-08-31 12:45:32 +0530

    our service section completed

--- ac-service/src/app/page.tsx
+++ ac-service/src/app/page.tsx
@@ -1,103 +1,155 @@
 import Image from "next/image";
+import { FaMoneyBillAlt, FaHandshake, FaCalendarCheck } from "react-icons/fa";
+import { BsEmojiSmileFill } from "react-icons/bs";
+import { FaPeopleCarryBox } from "react-icons/fa6";
+import WelcomeBanner from "../../public/images/banner/welcome-banner.jpeg";
+import ACServiceImg from "../../public/images/banner/service-1.webp";
+import WashingMechineService from "../../public/images/banner/service-2.webp";
+import DTHService from "../../public/images/banner/service-3.webp";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <div className="mx-auto px-4 sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1170px]">
+        <section className="card-container">
+          <div className="flex flex-col md:flex-row justify-between gap-1">
+            <div className="w-full md:w-55 bg-[#09aeb8] py-[60px] px-[20px] text-center text-white transition delay-50 duration-300 ease-in-out hover:-translate-y-2 hover:bg-[#FFD763] ">
+              <p className="flex justify-center text-[60px] mb-5">
+                <FaMoneyBillAlt />
+              </p>
+              <p className="text-[20px]">No Upfront Payments</p>
+            </div>
+            <div className="w-full md:w-55 bg-[#09aeb8] py-[60px] px-[20px] text-center text-white transition delay-50 duration-300 ease-in-out hover:-translate-y-2 hover:bg-[#FFD763]">
+              <p className="flex justify-center text-[60px] mb-5">
+                <BsEmojiSmileFill className="text-white" />
+              </p>
+              <p className="text-[20px]">Satisfaction Guarantee</p>
+            </div>
+            <div className="w-full md:w-55 bg-[#09aeb8] py-[60px] px-[20px] text-center text-white transition delay-50 duration-300 ease-in-out hover:-translate-y-2 hover:bg-[#FFD763]">
+              <p className="flex justify-center text-[60px] mb-5">
+                <FaCalendarCheck />
+              </p>
+              <p className="text-[20px]">
+                Emergency <br /> Service
+              </p>
+            </div>
+            <div className="w-full md:w-55 bg-[#09aeb8] py-[60px] px-[20px] text-center text-white transition delay-50 duration-300 ease-in-out hover:-translate-y-2 hover:bg-[#FFD763]">
+              <p className="flex justify-center text-[60px] mb-5">
+                <FaHandshake />
+              </p>
+              <p className="text-[20px]">Fixed Right Promise</p>
+            </div>
+            <div className="w-full md:w-55 bg-[#09aeb8] py-[60px] px-[20px] text-center text-white transition delay-50 duration-300 ease-in-out hover:-translate-y-2 hover:bg-[#FFD763]">
+              <p className="flex justify-center text-[60px] mb-5">
+                <FaPeopleCarryBox />
+              </p>
+              <p className="text-[20px]">
+                8 Years <br /> Experience
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div className="mx-auto px-4 sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1170px]">
+        <section className="welcome-banner-container mt-15">
+          <div className="flex flex-col md:flex-row justify-between gap-16">
+            <div className="w-full md:w-2.5/5">
+              <Image src={WelcomeBanner} alt="welocme-banner"></Image>
+            </div>
+            <div className="w-full md:w-2.3/5">
+              <h4 className="text-[#38373c] font-medium text-[20px] relative before:content-[''] before:absolute before:left-0 before:top-0 before:h-[3px] before:w-[25px] before:bg-[#38adb6] pt-5">
+                Welcome to loazzne
+              </h4>
+              <h2 className="text-[30px] font-light mt-8 leading-[36px]">
+                Heating and air conditioning repair and installation company
+              </h2>
+              <p className="text-[#87868a] leading-[34px] text-[18px] mt-10 mb-10">
+                There are many variations of passages of lorem ipsum available,
+                but the majorify have suffered alteration in some form by
+                injected humour, or randmised words which don&apos;t look even
+                slightly believable.
+              </p>
+              <Link
+                href="/"
+                className="bg-[#ffd763] text-[#38373c] px-10 py-[17px] inline-block font-bold no-underline tracking-[1px] text-[12px] hover:bg-[#09aeb8] hover:text-white transition-colors delay-50"
+              >
+                Learn More
+              </Link>
+            </div>
+          </div>
+        </section>
+      </div>
+      <section className="our-services-container bg-[#f1f1f1] pt-[40px] pb-[40px] sm:pt-[120px] sm:pb-[120px] mt-15 text-center">
+        <div className="mx-auto px-4 sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1170px]">
+          <h1 className="text-[40px] font-light text-[#38373c] leading-[30px] mb-[30px] sm:text-[54px] sm:leading-[54px] sm:mb-[60px]">
+            Our Services
+          </h1>
+          <div className="services-box flex flex-col md:flex-row justify-between gap-8">
+            <div className="group bg-white transition duration-300 border-[10px] border-white text-center w-full sm:w-1/3 hover:bg-[#38373c]">
+              <Image
+                src={ACServiceImg}
+                alt=""
+                className="transition duration-300 group-hover:opacity-20"
+              />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+              <h2 className="mb-10 text-[24px] font-semibold pt-10 group-hover:text-[#f5d974] transition duration-300">
+                Cooling Services
+              </h2>
+              <p className="text-[#87868a] text-[16px] mb-10 font-normal w-[83%] mx-auto group-hover:text-white transition duration-300">
+                There are many variations passages of lorem ipsum available but
+                the majority have suffered alteration.
+              </p>
+              <Link
+                href="/"
+                className="inline-block font-bold text-[#38373c] px-10 py-[17px] text-[12px] tracking-[1px] no-underline bg-[#F1F1F1] w-full group-hover:text-black group-hover:bg-[#f5d974] transition duration-300"
+              >
+                Learn More
+              </Link>
+            </div>
+            <div className="group bg-white transition duration-300 border-[10px] border-white text-center w-full sm:w-1/3 hover:bg-[#38373c]">
+              <Image
+                src={WashingMechineService}
+                alt=""
+                className="transition duration-300 group-hover:opacity-20"
+              />
+              <h2 className="mb-10 text-[24px] font-semibold pt-10 group-hover:text-[#f5d974] transition duration-300">
+                Heating Services
+              </h2>
+              <p className="text-[#87868a] text-[16px] mb-10 font-normal w-[83%] mx-auto group-hover:text-white transition duration-300">
+                There are many variations passages of lorem ipsum available but
+                the majority have suffered alteration.
+              </p>
+              <Link
+                href="/"
+                className="inline-block font-bold text-[#38373c] px-10 py-[17px] text-[12px] tracking-[1px] no-underline bg-[#F1F1F1] w-full group-hover:text-black group-hover:bg-[#f5d974] transition duration-300"
+              >
+                Learn More
+              </Link>
+            </div>
+            <div className="group bg-white transition duration-300 border-[10px] border-white text-center w-full sm:w-1/3 hover:bg-[#38373c]">
+              <Image
+                src={DTHService}
+                alt=""
+                className="transition duration-300 group-hover:opacity-20"
+              />
+              <h2 className="mb-10 text-[24px] font-semibold pt-10 group-hover:text-[#f5d974] transition duration-300">
+                Other Services
+              </h2>
+              <p className="text-[#87868a] text-[16px] mb-10 font-normal w-[83%] mx-auto group-hover:text-white transition duration-300">
+                There are many variations passages of lorem ipsum available but
+                the majority have suffered alteration.
+              </p>
+              <Link
+                href="/"
+                className="inline-block font-bold text-[#38373c] px-10 py-[17px] text-[12px] tracking-[1px] no-underline bg-[#F1F1F1] w-full group-hover:text-black group-hover:bg-[#f5d974] transition duration-300"
+              >
+                Learn More
+              </Link>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+    </>
   );
 }
